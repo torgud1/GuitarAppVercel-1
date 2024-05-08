16dc065db0528b38c734f6347776ac42213381bf
noroff-bed1 <de-contact@noroff.no>
2024-05-08 14:40:49 +0200

    Updated dialectmodel

--- models/index.js
+++ models/index.js
@@ -10,8 +10,8 @@ const connection = {
   host: process.env.HOST,
   port: process.env.DB_PORT,
   dialect: process.env.DIALECT,
-  dialectmodel: process.env.DIALECTMODEL,
-//   dialectmodel: require('mysql2'),
+  // dialectmodel: process.env.DIALECTMODEL,
+  dialectmodel: require('mysql2'),
 };
 const sequelize = new Sequelize(connection);
 const db = {}
