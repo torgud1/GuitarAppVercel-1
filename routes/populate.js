5747ca458150a0d5763e72e79e9dc015be1f091d
noroff-bed1 <de-contact@noroff.no>
2024-05-10 07:56:39 +0200

    Updated some configs

--- routes/populate.js
+++ routes/populate.js
@@ -7,6 +7,7 @@ var populateService = new PopulateService(db)
 //Populate DB
 router.post('/', async function (req, res, next) {
     await populateService.populateDatabase();
+    res.status(200).json({ success: true, message: "Database populated successfully." });
     res.end();
 })
 
